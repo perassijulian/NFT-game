a80363b8a4780d840e529cd714df59b7bc9234d8
Julian Perassi <perassijulian@gmail.com>
2022-01-12 13:23:21 -0300

    add mint your weapon component

--- src/Components/SelectCharacter/index.jsx
+++ src/Components/SelectCharacter/index.jsx
@@ -1,13 +1,92 @@
 import React, { useEffect, useState } from 'react';
 import './SelectCharacter.css';
+import { ethers } from 'ethers';
+import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import MultiMetaverse from '../../utils/MultiMetaverse.json';
 
-/*
- * Don't worry about setCharacterNFT just yet, we will talk about it soon!
- */
 const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+
+  const renderCharacters = () =>
+    characters.map((character, index) => (
+      <div className="character-item" key={character.name}>
+        <div className="name-container">
+          <p>{character.name}</p>
+        </div>
+        <img src={character.imageURI} alt={character.name} />
+        <button
+          type="button"
+          className="character-mint-button"
+          //onClick={mintCharacterNFTAction(index)}
+        >{`Mint ${character.name}`}</button>
+      </div>
+    ));
+
+  useEffect(() => {
+    const { ethereum } = window;
+  
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        MultiMetaverse.abi,
+        signer
+      );
+  
+      /*
+       * This is the big difference. Set our gameContract in state.
+       */
+      setGameContract(gameContract);
+    } else {
+      console.log('Ethereum object not found');
+    }
+  }, []);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        console.log('Getting contract weapons to mint');
+  
+        /*
+         * Call contract to get all mint-able characters
+         */
+        const charactersTxn = await gameContract.getAllDefaultWeapons();
+        console.log('charactersTxn:', charactersTxn);
+  
+        /*
+         * Go through all of our characters and transform the data
+         */
+        const characters = charactersTxn.map((characterData) =>
+          transformCharacterData(characterData)
+        );
+  
+        /*
+         * Set all mint-able characters in state
+         */
+        setCharacters(characters);
+      } catch (error) {
+        console.error('Something went wrong fetching weapons:', error);
+      }
+    };
+  
+    /*
+     * If our gameContract is ready, let's get characters!
+     */
+    if (gameContract) {
+      getCharacters();
+    }
+  }, [gameContract]);
+
+
   return (
     <div className="select-character-container">
-      <h2>Mint Your Hero. Choose wisely.</h2>
+      <h2>Mint Your Weapon. Choose wisely.</h2>
+      {/* Only show this when there are characters in state */}
+      {characters.length > 0 && (
+        <div className="character-grid">{renderCharacters()}</div>
+      )}
     </div>
   );
 };
